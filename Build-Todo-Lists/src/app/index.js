0d5307c191a0bc8594f46d25e0fe622e22d292b6
Krittiya Clark <krittiyaclark@gmail.com>
2017-08-15 17:39:50 -0400

    Added .gitignore.

--- Build-Todo-Lists/src/app/index.js
+++ Build-Todo-Lists/src/app/index.js
@@ -1,1 +1,14 @@
-alert("It works!");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Create component
+var TodoCompnent = React.createClass({
+    render: function(){
+        return(
+            <h1>Hello</h1>
+        );
+    }
+});
+
+// Put component into html page
+ReactDOM.render(<TodoCompnent />, document.getElementById('todo-wrapper'));
