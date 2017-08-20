be036e480e21d037cd850144441dfa4d55b4c00f
Krittiya Clark <krittiyaclark@gmail.com>
2017-08-20 16:53:32 -0400

    Create a component

--- Build-Todo-Lists/src/app/index.js
+++ Build-Todo-Lists/src/app/index.js
@@ -1,14 +1,33 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+//import { Router, Route, browserHistory, Link } from 'react-router';
 
-//Create component
-var TodoCompnent = React.createClass({
-    render: function(){
+
+//Create a component
+var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: ['wash up', 'et some cheese', 'play sketboard']
+        }
+    }, //getInitialState
+    
+    render: function() {
+        var todos = this.state.todos;
+        todos = todos.map(function(item, index){
+            return(TodoItem key={index} item={item} onDelete={this.onDelete} />);
+        }.bind(this));
         return(
-            <h1>Hello</h1>
+            <div id="todo-list">
+                 <link to={"/about"}>About</link>
+                 <p>The busiest people have the most leisure..</p>
+                 <ul>{todos}</ukl>
+                 <addItem onAdd={this.onAdd} />    
+            </div>
+
         );
-    }
+    }, //render
 });
 
-// Put component into html page
-ReactDOM.render(<TodoCompnent />, document.getElementById('todo-wrapper'));
+      
+ReactDOM.render(<App />, document.getElementById('todo-wrapper'));
+
