ca6d3e87d9f424c702853fe73ef40c9546060592
Krittiya Clark <krittiyaclark@gmail.com>
2017-08-20 22:45:14 -0400

    onDelete and  onAdd functions

--- Build-Todo-Lists/src/app/index.js
+++ Build-Todo-Lists/src/app/index.js
@@ -14,18 +14,35 @@ var TodoComponent = React.createClass({
     render: function() {
         var todos = this.state.todos;
         todos = todos.map(function(item, index){
-            return(TodoItem key={index} item={item} onDelete={this.onDelete} />);
+            return(<TodoItem key={index} item={item} onDelete={this.onDelete} />);
         }.bind(this));
         return(
             <div id="todo-list">
                  <link to={"/about"}>About</link>
                  <p>The busiest people have the most leisure..</p>
-                 <ul>{todos}</ukl>
-                 <addItem onAdd={this.onAdd} />    
+                 <ul>{todos}</ul>
+                 <AddItem onAdd={this.onAdd} />    
             </div>
-
         );
     }, //render
+
+    //Custom functions
+    onDelete: function(item){
+        var updatedTodos = this.state.todos.filter(function(val, index){
+            return item !==val;
+        });
+        this.setState({
+            todos: updatedTodos
+        });
+    },
+
+    onAdd: function(item){
+        var updatedTodos = this.state.todos;
+        updatedTodos.push(item);
+        this.setState({
+            todos: updatedTodos
+        })
+    }
 });
 
       
